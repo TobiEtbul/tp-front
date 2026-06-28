5dc3e9d2097384532601f74a4b64d25202708fa4
TobiEtbul <etbultobias@gmail.com>
2026-06-28 20:14:12 -0300

    Configurar ESLint con reglas de TypeScript y React
    Closes #1

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-key': 'error',
+      'no-console': 'warn',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+];
